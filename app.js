3d7ce148d5115db322b4c3cddfba09972396fa86
elijahmintzer <47578341+elijahmintzer@users.noreply.github.com>
2020-07-04 21:29:14 -0400

    initial commit
    have most stuff done, just have to display it on website

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const fetch = require('node-fetch');
+require('dotenv-flow').config();
+
+const config = {
+    apikey: process.env.APIKEY,
+    port: process.env.PORT
+}
+
+const port = config.port || 1337;
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.use(express.static('public'));
+app.use(express.json({limit: '1mb'}));
+
+app.post('/api', (request, response) => {
+    const data = request.body;
+    data.timestamp = timestamp;
+    response.json({
+        status: 'success',
+    });
+});
+
+app.get('/steamapi/:steamid', async (request, response) => {
+    const steamid = request.params.steamid;
+    const api_url = `https://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v2/?appid=730&key=${config.apikey}&steamid=${steamid}`;
+    const fetch_response = await fetch(api_url);
+    const json = await fetch_response.json();
+    response.json(json);
+});
