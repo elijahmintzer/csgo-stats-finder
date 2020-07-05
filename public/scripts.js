90f50c5a897469061eb56903c788e316cde59e86
elijahmintzer <47578341+elijahmintzer@users.noreply.github.com>
2020-07-04 23:17:50 -0400

    shows stats on page
    currently shows Kill-Death Ratio, Total Deaths and Total Kills.

--- public/scripts.js
+++ public/scripts.js
@@ -7,4 +7,12 @@ async function getStats(){
     const api_url = `/steamapi/${steamid}`;
     const response = await fetch(api_url);
     const json = await response.json();
+    const totalkills = json.playerstats.stats[0].value;
+    const totaldeaths = json.playerstats.stats[1].value;
+    const kdratio = totalkills / totaldeaths;
+
+    document.getElementById('kdratio').textContent = kdratio.toFixed(2);
+    document.getElementById('totalkills').textContent = totalkills;
+    document.getElementById('totaldeaths').textContent = totaldeaths;
 }
+
